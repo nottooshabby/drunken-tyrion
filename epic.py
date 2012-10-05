c88883d075a38c3dce140b7cf793c62972ae9f29
nottooshabby <fireworks0496@yahoo.com>
2012-10-04 22:10:43 -0500

    blah

--- epic.py
+++ epic.py
@@ -1,2 +1,16 @@
+import random
+
 __author__ = 'jonathonfuller'
 print ('Hello world!')
+userChoice = input('What do you choose? Rock, paper, or scissors?')
+userChoice = str(userChoice)
+computerChoice = random.random()
+if computerChoice < .34:
+    computerChoice =  'rock'
+elif computerChoice >=.34 and computerChoice < .64:
+    computerChoice =  'scissors'
+else:
+    computerChoice = 'paper'
+if userChoice == computerChoice:
+    print('It is a tie!')
+print(computerChoice)
