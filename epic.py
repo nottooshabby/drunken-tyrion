02d04a4942e301e08dc8e5b43ed2f5a2cfb0439d
nottooshabby <fireworks0496@yahoo.com>
2012-10-05 11:18:06 -0500

    Update epic.py

--- epic.py
+++ epic.py
@@ -1,5 +1,9 @@
-import random
+#My first game of rock, paper and scissors!
+#Made by Jonathon Fuller
+#If I could set this as a function, I could set the variables within the function as userChoice and computerChoice
+#and I could set a limit, or infinate, based on user input. I also need to return a wrong entry with 'Please re-enter your #choice!'
 
+import random
 __author__ = 'jonathonfuller'
 print ('Hello world!')
 userChoice = input('What do you choose? Rock, paper, or scissors?')
@@ -11,9 +15,25 @@ elif computerChoice >=.34 and computerChoice < .64:
     computerChoice =  'scissors'
 else:
     computerChoice = 'paper'
+print('The computer has chosen: ' + computerChoice)
 if userChoice == computerChoice:
     print('It is a tie!')
-    if userChoice = 'rock' and computerChoice = 'paper':
-        print('The computer has won!')
-    if userChoice = 'scissors' and computerChoice = 'paper':
-        print('You have won!')
+    return tieRound
+if userChoice = 'rock':
+    if computerChoice = 'scissors':
+         print('You win!')
+    else:
+         print('The computer has won!)
+if userChoice = 'paper':
+   if computerChoice = 'rock':
+      print('You win!')
+   else:
+      print('The computer has won!')
+if userChoice = 'scissors':
+   if computerChoice = 'rock':
+      print('The computer has won!')
+   else:
+      print ('You win!')
+return secondRound
+
+    
